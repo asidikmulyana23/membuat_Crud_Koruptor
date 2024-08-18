d39dc67cbe1379499fdeeabb6f4458fc6e0ed750
asidikmulyana23 <sidikmulyana000@gmail.com>
2024-08-18 23:44:11 +0700

    CRUD Laravel 11 For Fun

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Crud Laravel 11</title>
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}" >
+</head>
+<body>
+    
+    <h1 class="text-center mt-3">Selamat datang di aplikasi CRUD Laravel 11</h1>
+
+    <div class="text-center">
+        <a href="{{route('siswa.tampil')}}" class="btn btn-primary">Kelola Koruptor</a>
+    </div>
+</body>
+</html>
